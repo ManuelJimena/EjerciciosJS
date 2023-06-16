5f79c63f366b8c239972d48fa56b2cc8889bf8e9
Admin <Manuel.jimena29@gmail.com>
2023-06-16 11:02:47 +0200

    refact ejercicios 7 y 8

--- ejercicio08.js
+++ ejercicio08.js
@@ -1,9 +1,9 @@
 //Completa la función que tomando dos números como argumento devuelva el más alto.
 function sum(numberOne, numberTwo) {
     if (numberOne > numberTwo) {
-      return numberOne;
+      console.log(numberOne);
     } else {
-      return numberTwo;
+      console.log(numberTwo);
     }
   }
-  console.log(sum(15, 3));
+  (sum(15, 3));
