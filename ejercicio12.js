b5552344e952dbae2a39a4e394c06192e35defca
Admin <Manuel.jimena29@gmail.com>
2023-06-23 12:40:30 +0200

    refact corregido ejercicio 12

--- ejercicio12.js
+++ ejercicio12.js
@@ -5,7 +5,7 @@ const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 function averageWord(param) {
 let total = 0;
 for (const element of param) {
-    if (typeof element === "string)") {
+    if (typeof element === "string") {
         total += element.length;
     } else {
         total += element;
