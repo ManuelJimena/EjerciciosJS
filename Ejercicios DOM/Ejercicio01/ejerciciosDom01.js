39ba5f51b75c1dac53d51667c85df496e83ea003
Admin <Manuel.jimena29@gmail.com>
2023-07-06 14:12:32 +0200

    Refact ejercicio 1.5

--- Ejercicios DOM/Ejercicio01/ejerciciosDom01.js
+++ Ejercicios DOM/Ejercicio01/ejerciciosDom01.js
@@ -1,15 +1,29 @@
 /* 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme*/
+
 const button = document.querySelector('.showme');
+console.log(button);
 
 /* 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado*/
 
+const myH1 = document.querySelector("#pillado");
+console.log(myH1);
+
 /* 1.3 Usa querySelector para mostrar por consola todos los p*/
 
+const p = document.querySelectorAll("p");
+console.log(p);
+
 /* 1.4 Usa querySelector para mostrar por consola todos los elementos con
-la clase.pokemon/*
+la clase.pokemon*/
+
+const h4 = document.querySelectorAll('.pokemon')
+console.log(h4);
 
 /* 1.5 Usa querySelector para mostrar por consola todos los elementos con
 el atributo data-function="testMe".*/
+
+const span = document.querySelectorAll('testMe')
+console.log(span);
 
 /* 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo
 data-function="testMe".*/
